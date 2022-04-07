f0dfb250522036fa5996deaf01724cf4c0f18d65
Resul Takak <resultakak@gmail.com>
2022-04-07 22:10:50 +0300

    home.html template eklendi

--- backend/src/app/views.py
+++ backend/src/app/views.py
@@ -1,15 +1,5 @@
-#from django.shortcuts import render
-
-
-
-# Create your views here.
-
-
-from django.http import HttpResponse
-import datetime
+from django.shortcuts import render
 
 
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now {0}.</body></html>".format(now)
-    return HttpResponse(html)
+    return render(request, 'home.html', {})
