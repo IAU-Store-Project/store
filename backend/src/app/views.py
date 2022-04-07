94d13f823292db40321a6ccd3c9eb22ff0c4ed7e
Resul Takak <resultakak@gmail.com>
2022-04-07 21:53:30 +0300

    home view oluşturuldu

--- backend/src/app/views.py
+++ backend/src/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+
+
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+import datetime
+
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now {0}.</body></html>".format(now)
+    return HttpResponse(html)
