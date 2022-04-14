e90ba3685a2c72724d27c8c896126654ccf24f3e
Resul Takak <resultakak@gmail.com>
2022-04-14 21:48:53 +0300

    Sepet uygulamasi eklendi

---
+++ backend/src/basket/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from product.models import Product
+
+User = get_user_model()
+
+
+class Basket(models.Model):
+    customer = models.OneToOneField(User)
+    total = models.CharField()
+
+
+class BasketProduct(models.Model):
+    basket = models.ForeignKey(Basket)
+    product = models.ForeignKey(Product)
+    qty = models.CharField()
+
+
