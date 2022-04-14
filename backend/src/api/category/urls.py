0f7306cb67e16d2112c360093678876b8c38a616
Resul Takak <resultakak@gmail.com>
2022-04-14 21:47:22 +0300

    Kategori uygulaması eklendi

---
+++ backend/src/api/category/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from api.category.resources import (
+    CategoryListCreateAPIView,
+    CategoryRetrieveUpdateDestroyAPIView
+)
+
+urlpatterns = [
+    path('', CategoryListCreateAPIView.as_view(), name='category-api-list'),
+    path('/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-api-crud'),
+]
