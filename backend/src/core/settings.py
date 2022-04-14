a0d57a0188ddfcd8e4ef2d2a3c1d39186471975a
Resul Takak <resultakak@gmail.com>
2022-04-14 21:51:33 +0300

    Kutuphaneler eklendi

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig'
+    'django_extensions',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
