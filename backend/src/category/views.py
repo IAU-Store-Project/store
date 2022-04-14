8d84b28c81072f6e8e8045b7bbb7608b83d90a72
Resul Takak <resultakak@gmail.com>
2022-04-14 23:53:27 +0300

    Category uygulamasina Class Based View Eklendi

--- backend/src/category/views.py
+++ backend/src/category/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render, redirect
 from category.forms import NewCategoryForm
 from category.models import Category
+from django.views.generic import ListView
 
 
-def categories(request):
-    categories = Category.objects.all()
-    context = {"title": "Kategoriler", "categories": categories}
-    return render(request, 'categories.html', context=context)
+class CategoryListView(ListView):
+    model = Category
+    queryset = Category.objects.all().order_by('-id')
+    context_object_name = 'categories'
+    template_name = 'categories.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Kategoriler'
+        return context
+
+
+# Detail View
+
+
+
+
+# def categories(request):
+#     categories = Category.objects.all()
+#     context = {"title": "Kategoriler", "categories": categories}
+#     return render(request, 'categories.html', context=context)
+
+
+# Create View
 
 def new_category(request):
     form = NewCategoryForm(request.POST or None)
