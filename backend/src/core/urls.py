6f44b3a670f1c527f7c602571f2d0f1e491ab29e
Resul Takak <resultakak@gmail.com>
2022-04-14 22:39:04 +0300

    TemplateView kullanildi

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('app.urls')),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name="about.html")),
 ]
