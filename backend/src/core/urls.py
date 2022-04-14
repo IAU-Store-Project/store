1212c6e4e17e5c07d12685103980f58942b57580
Resul Takak <resultakak@gmail.com>
2022-04-14 21:51:06 +0300

    Update urls.py

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from app.views import home
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', include('app.urls')),
+    path('categories/', include('category.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
