ce266aa2a1a4df9f918219e1e13eea3b9c546d2f
Resul Takak <resultakak@gmail.com>
2022-04-14 23:47:43 +0300

    API Token ile Auth mekanizmasi eklendi

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
-    path('categories/', include('api.category.urls'))
+    path('categories/', include('api.category.urls')),
+    path('auth/', views.obtain_auth_token)
 ]
