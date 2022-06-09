27821a62c6abd7181c0ff05497f93ef20ac556e7
Resul Takak <resultakak@gmail.com>
2022-06-09 21:09:10 +0300

    Create config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+	// baseURL: `https://iau-store.com/api/`,
+	baseURL: `http://127.0.0.1:8000/api/`,
+	constants: {
+		HOME: "/",
+		DEFAULT: "/user",
+		REDIRECT_PATH: "/signin",
+	}
+};
+
+export default config;
