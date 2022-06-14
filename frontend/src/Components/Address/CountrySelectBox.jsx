5b8c3adb48dcf6b84614dfd641efc35c835fc429
Resul Takak <resultakak@gmail.com>
2022-06-14 23:11:44 +0300

    Create CountrySelectBox.jsx

---
+++ frontend/src/Components/Address/CountrySelectBox.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const CountrySelectBox = ({ address, handleChange, countries }) => {
+	return (
+			<select name="country"
+							id="country"
+							className="form-select"
+							value={address.country}
+							onChange={handleChange}
+							required={true}>
+				<option key={`cs-0`}></option>
+				{countries && countries?.map(country => (
+						<option
+								key={`cs-${country.id}`}
+								value={`${country.id}`}>
+							{country.name}
+						</option>
+				))}
+			</select>
+	);
+};
+
+CountrySelectBox.propTypes = {
+	address: PropTypes.object.isRequired,
+	handleChange: PropTypes.func.isRequired,
+	country: PropTypes.object.isRequired,
+};
+
+export default CountrySelectBox;
