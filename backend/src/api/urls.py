65b6baa23ea3b4088a41675989fd8dccd45f7c50
Resul Takak <resultakak@gmail.com>
2022-06-22 00:52:56 +0300

    Update urls.py

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
 # from api.views import ApiRoot
+from api.resources import PayAPIView
 from basket.resources import CheckoutAPIView
 
 urlpatterns = [
@@ -14,6 +15,7 @@ urlpatterns = [
     path('addresses/', include('address.api')),
     path('orders/', include('order.api')),
     path('checkout/', CheckoutAPIView.as_view()),
+    path('pay/', PayAPIView.as_view()),
     path('openapi', get_schema_view(
         title="IAU Store API",
         description="IAU Store API for E-Commerce Frontend",
