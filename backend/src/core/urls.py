95d92e66a0e51da2046da7022887fa72e59393ea
Resul Takak <resultakak@gmail.com>
2022-06-22 01:43:27 +0300

    Update urls.py

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -18,7 +18,9 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
-
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from core.schema import schema
 
 admin.site.site_header = 'Store Administrator'
 admin.site.index_title = 'Management'
@@ -33,6 +35,7 @@ urlpatterns = [
     path('', RedirectView.as_view(pattern_name='home')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     path('app/product/', include('product.urls')),
