31c7b8e0a8338c5346266184be3d43c213e6a6bb
Resul Takak <resultakak@gmail.com>
2022-06-22 00:16:40 +0300

    Update tasks.py

--- backend/src/app/tasks.py
+++ backend/src/app/tasks.py
@@ -1,6 +1,23 @@
+import requests
 from celery import shared_task
+from django.conf import settings
 
+TELEGRAM_TOKEN = getattr(settings, 'TELEGRAM_TOKEN', None)
+TELEGRAM_CHAT_ID = getattr(settings, 'TELEGRAM_CHAT_ID', None)
 
 @shared_task(bind=True)
-def hello_world(self):
-    return "Hello"
+def telegram_sender(self, text):
+    url = "https://api.telegram.org/bot{0}/sendMessage".format(TELEGRAM_TOKEN)
+    payload = "chat_id={0}&text={1}".format(TELEGRAM_CHAT_ID, text)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.request(
+        "POST",
+        url,
+        headers=headers,
+        data=payload
+    )
+
+    return response.text
