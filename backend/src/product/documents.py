863edbc1262c714d93dfbecae6fff9f68640413d
Resul Takak <resultakak@gmail.com>
2022-06-26 03:45:32 +0300

    Update documents.py

--- backend/src/product/documents.py
+++ backend/src/product/documents.py
@@ -1,12 +1,15 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-
 from product.models import Product
+from django.utils.text import slugify
+from django.conf import settings
+
+DEBUG = getattr(settings, 'DEBUG', None)
 
 
 @registry.register_document
 class ProductDocument(Document):
-    pk = fields.IntegerField()
+    id = fields.IntegerField()
     brand = fields.ObjectField(
         attr='brand',
         properties={
@@ -27,11 +30,26 @@ class ProductDocument(Document):
             'suggest': fields.CompletionField(),
         }
     )
+    slug = fields.TextField()
     sku = fields.TextField()
     price = fields.FloatField()
     stock = fields.TextField()
     unit = fields.TextField()
-    img = fields.TextField(attr='img.url')
+    img = fields.FileField()
+
+    def prepare_img(self, instance):
+        if DEBUG == True:
+            return "{}{}".format(
+                'http://127.0.0.1:8000',
+                instance.img.url if instance.img else ''
+            )
+
+        return instance.img.url if instance.img else ''
+
+    def prepare_slug(self, instance):
+        title = slugify(instance.title)
+        title = '-'.join([x.capitalize() for x in title.split('-')])
+        return title
 
     class Index:
         name = 'products'
